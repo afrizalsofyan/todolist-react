658251b40f5d5e38e86915b2e72cd1bf2d3fc345
afrizalsofyan <afrizalafaandy@gmail.com>
2022-11-02 23:35:38 +0700

    chore: init project

---
+++ src/pages/Dashboard.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Dashboard() {
+  return (
+    <section data-cy="dashboard-data">
+      <header data-cy="header-layout"></header>
+    </section>
+  );
+}
+
+export default Dashboard;
